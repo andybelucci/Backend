e3816eaf866dd53f963190c379556e5f80eab6d3
andybelucci <beluccianderson@gmail.com>
2023-08-12 11:09:20 -0300

    Get para Livro por Id

---
+++ src/main/java/com/anderson/booksale/service/LivroService.java
@@ -0,0 +1,21 @@
+package com.anderson.booksale.service;
+
+import com.anderson.booksale.domain.Livro;
+import com.anderson.booksale.repositories.LivroRepository;
+import com.anderson.booksale.service.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class LivroService {
+
+    @Autowired
+    private LivroRepository repository;
+
+    public Livro findById(Integer id) {
+        Optional<Livro> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Livro.class.getName()));
+    }
+}
