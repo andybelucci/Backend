cfdb06046f03a7eb6b9c045a833287062edda1fa
andybelucci <beluccianderson@gmail.com>
2023-08-12 21:05:59 -0300

    Listando todos os livros por Categoria

---
+++ src/main/java/com/anderson/booksale/dtos/LivroDTO.java
@@ -0,0 +1,38 @@
+package com.anderson.booksale.dtos;
+
+import com.anderson.booksale.domain.Livro;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class LivroDTO implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String titulo;
+
+    public LivroDTO() {
+    }
+
+    public LivroDTO(Livro obj) {
+        this.id = obj.getId();
+        this.titulo = obj.getTitulo();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+}
